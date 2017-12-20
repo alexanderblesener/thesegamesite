4b4e114e964e6efedf3b197bf262421e61bb1d60
alexanderblesener <34605391+alexanderblesener@users.noreply.github.com>
2017-12-20 16:32:17 -0600

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,11 @@
+﻿<php?
+if (empty($_POST['comments']))
+{return false; }
+$message = strip_tags(htmlspecialchars($_POST['comments']));
+$to = "alexanderblesener@yahoo.com"; 
+$email_subject = "comments and suggestions";
+$email_body = "\n$message";
+$headers = "From: THESEGAMES@suggestion.com\n";
+mail($to,$email_subject, $email_body,$headers);
+return true;
+?>
